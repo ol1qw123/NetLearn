65976ddf1a0ea9e8e37c48e1f78f881749c267ed
Rashed Aljahdali <fmothoughts@gmail.com>
2024-11-30 08:26:55 +0300

    The use can input the address of the server itself.

--- src/uit.rs
+++ src/uit.rs
@@ -4,7 +4,6 @@ pub async fn start() -> Result<() , Box<dyn std::error::Error>> {
     println!("Enter the address and the port as in (192.168.1.1:6666)");
 
     let mut buffer_for_addr = String::new();
-
     
     let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
 
@@ -13,11 +12,11 @@ pub async fn start() -> Result<() , Box<dyn std::error::Error>> {
 
     
     let address = buffer_for_addr.trim();
-    let tcp_accpetor =
+    let tcp_acceptor =
         tokio::net::TcpListener::bind(address).await?;
     println!("It's listening for connections in {}." , address);
     loop {
-        let (mut socket , addr) = tcp_accpetor.accept().await?;
+        let (mut socket , addr) = tcp_acceptor.accept().await?;
         println!("Connection received from {}", addr);
         tokio::spawn(async move {
             let mut buf = [0; 1024];
@@ -32,6 +31,14 @@ pub async fn start() -> Result<() , Box<dyn std::error::Error>> {
                         return;
                     }
                 };
+                println!("read {} bytes from {}", n, addr);
+                let data = String::from_utf8_lossy(&buf[0..n]);
+                println!("{}", data);
+                //println!("What is the responed ????");
+                //let mut buffer_for_responed = String::new();
+                //let mut readerforresponse = tokio::io::BufReader::new(tokio::io::stdin());
+                //readerforresponse.read_line(&mut buffer_for_responed).await.expect("TODO: panic message");
+                //let mut bytes = buffer_for_responed.as_bytes();
                 if let Err(e) = socket.write_all(&buf[0..n]).await {
                     eprintln!("Failed to write to socket: {}", e);
                     return;
